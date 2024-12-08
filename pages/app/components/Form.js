e759d3419124a6729fe9535f8f7b75be655a76d5
ryangois <ryangoisdev@gmail.com>
2024-12-08 17:04:59 -0300

    Form changes

--- pages/app/components/Form.js
+++ pages/app/components/Form.js
@@ -6,6 +6,9 @@ import styles from '../styles/Form.module.css';
 // Dynamically import the FormComponent to disable SSR for this component
 function processForm(formData) {
   const userFormData = {
+    completeName: formData.completeName,
+    email: formData.email,
+    // cpf: formData.cpf,
     protocol: formData.protocol,
     goal: formData.goal,
     stateImportant: formData.stateImportant,
@@ -30,13 +33,12 @@ function processForm(formData) {
     monthlyContribution: formData.monthlyContribution,
     advisoryPhase: formData.advisoryPhase,
     additionalInfo: formData.additionalInfo,
-    email: formData.email
   };
 
   const filteredData = Object.fromEntries(
     Object.entries(userFormData).filter(([key, value]) => value !== undefined && value !== "")
   );
-  
+
   console.log(filteredData);
 
   return filteredData;
@@ -45,6 +47,9 @@ function processForm(formData) {
 function formatFormDataWithBullets(formData) {
   const fieldLabels = {
     protocol: "Protocolo",
+    completeName: "• Nome Completo",
+    // cpf: "CPF",
+    email: "• E-mail",
     goal: "Objetivo",
     stateImportant: "Estado importante",
     selectedStates: "• Estados selecionados",
@@ -67,9 +72,9 @@ function formatFormDataWithBullets(formData) {
     monthlyContribution: "• Contribuição mensal",
     advisoryPhase: "• Fase de consultoria",
     additionalInfo: "• Informações adicionais",
-    email: "• E-mail"
+
   };
-  
+
 
   return Object.entries(formData)
     .map(([key, value]) => {
@@ -100,7 +105,7 @@ function formatFormDataWithBullets(formData) {
             // Verifica se o subValor é um array de objetos (como as cidades, bairros e distritos)
             if (Array.isArray(subValue)) {
               return `${label} (${subKey}):\n- ${subValue.map(item => item?.label || item).join('\n- ')}`;
-            }
+            } //pegar a subkey e fazer get na api de novo
             return null;
           })
           .filter(Boolean)  // Remove valores nulos
@@ -128,12 +133,15 @@ const FormComponent = dynamic(() => import('./FormComponent'), { ssr: false });
 
 export default function Form() {
   const [formData, setFormData] = useState({
+    completeName: '',
+    // cpf: '',
+    email: '',
     goal: '',
     stateImportant: '',
     selectedStates: [],
     selectedCities: {},
     selectedNeighborhoods: {},
-    selectedDistricts:{},
+    selectedDistricts: {},
     propertyType: [],
     minArea: '',
     maxArea: '',
@@ -151,16 +159,56 @@ export default function Form() {
     monthlyContribution: '',
     advisoryPhase: '',
     additionalInfo: '',
-    email: '',
   });
 
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState(null);
   const messageRef = useRef(null);
 
+  // function formatCPF(cpf) {
+  //   cpf = cpf.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+  //   if (cpf.length === 11) {
+  //     // Formata o CPF se tiver 11 números
+  //     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  //   }
+  //   return cpf; // Caso contrário, retorna o valor sem formatação
+  // }
+
+  function formatCurrency(value) {
+    if (isNaN(value) || value === null || value === '') return ''; // Verifica se o valor não é um número válido
+
+    // Se for válido, formata o valor como moeda
+    value = parseFloat(value).toFixed(0); // Remove os centavos
+    return `R$ ${value.replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`; // Formatação para adicionar pontos de milhar
+  }
+
+
   const handleInputChange = (name, value) => {
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    let formattedValue = value;
+
+    // Se o campo for 'cpf', aplica a formatação do CPF
+    if (name === 'cpf') {
+      formattedValue = formatCPF(value);
+    }
+    // Se for um campo monetário, tenta garantir que o valor é numérico
+    else if (['fgtsValue', 'availableFunds', 'monthlyContribution', 'institutionCreditValues'].includes(name)) {
+      // Remove qualquer caractere não numérico (como vírgulas ou pontos)
+      formattedValue = value.replace(/\D/g, '');
+
+      // Se o valor for um número, formata como moeda
+      if (formattedValue && !isNaN(formattedValue)) {
+        formattedValue = formatCurrency(parseFloat(formattedValue));
+      } else {
+        formattedValue = ''; // Caso contrário, deixa o valor em branco
+      }
+    }
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: formattedValue, // Atualiza o estado com o valor formatado
+    }));
   };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -194,29 +242,49 @@ export default function Form() {
     }
   };
 
-
   return (
     <form className={styles.formContainer} onSubmit={handleSubmit}>
       <h1 className={styles.formTitle}>Questionário de Intenção de Compra de Imóvel de Leilão</h1>
 
-      {message && (
-        <div
-        className={`${styles.message} ${styles[message.type]}`}
-        ref={messageRef} // Attach the ref to the message container
-        tabIndex="-1" // Ensure the message can receive focus
-      >
-        {message.text}
+
+      <div className={styles.formGroup}>
+        <label>Nome Completo</label>
+        <input
+          type="text"
+          placeholder="Insira seu nome completo"
+          value={formData.completeName}
+          onChange={(e) => handleInputChange('completeName', e.target.value)}
+          required
+        />
       </div>
-      )}
+      {/* <div className={styles.formGroup}>
+        <label>CPF</label>
+        <input
+          type="text"
+          placeholder="Insira seu CPF"
+          value={formData.cpf}
+          onChange={(e) => handleInputChange('cpf', e.target.value)}
 
-      {loading && <div className={styles.loading}>Enviando...</div>}
+          required
+        />
+      </div> */}
 
+      <div className={styles.formGroup}>
+        <label>E-mail para confirmação:</label>
+        <input
+          type="email"
+          value={formData.email}
+          onChange={(e) => handleInputChange('email', e.target.value)}
+          required
+        />
+      </div>
       {/* Objetivo */}
       <div className={styles.formGroup}>
         <label>Qual é o objetivo da sua arrematação?</label>
         <select
           onChange={(e) => handleInputChange("goal", e.target.value)}
           value={formData.goal}
+          required
         >
           <option value="">Selecione</option>
           <option value="moradia">Para moradia própria</option>
@@ -240,6 +308,7 @@ export default function Form() {
       <div className={styles.formGroup}>
         <label>O Estado do imóvel é importante para você?</label>
         <select
+          required
           onChange={(e) => handleInputChange('stateImportant', e.target.value)}
           value={formData.stateImportant}
         >
@@ -257,24 +326,30 @@ export default function Form() {
       <div className={styles.formGroup}>
         <label>Características do imóvel que você busca:</label>
         <div>
-          {['Casa', 'Apartamento', 'Terreno', 'Imóvel comercial', 'Prédio', 'Vaga de Garagem'].map(
-            (type) => (
-              <label key={type}>
-                <input
-                  type="checkbox"
-                  value={type}
-                  checked={formData.propertyType.includes(type)}
-                  onChange={(e) => {
-                    const selected = e.target.checked
+          {['Casa', 'Apartamento', 'Terreno', 'Imóvel comercial', 'Prédio', 'Indiferente'].map((type) => (
+            <label key={type}>
+              <input
+                type="checkbox"
+                value={type}
+                checked={formData.propertyType.includes(type)}
+                onChange={(e) => {
+                  if (type === 'Indiferente') {
+                    // Se "Indiferente" for marcado, desmarque os outros e marque "Indiferente"
+                    const newSelected = e.target.checked ? ['Indiferente'] : [];
+                    handleInputChange('propertyType', newSelected);
+                  } else {
+                    // Se "Indiferente" não estiver marcado, altere os outros tipos normalmente
+                    const newSelected = e.target.checked
                       ? [...formData.propertyType, type]
                       : formData.propertyType.filter((t) => t !== type);
-                    handleInputChange('propertyType', selected);
-                  }}
-                />
-                {type}
-              </label>
-            )
-          )}
+                    handleInputChange('propertyType', newSelected);
+                  }
+                }}
+                disabled={formData.propertyType.includes('Indiferente') && type !== 'Indiferente'}
+              />
+              {type}
+            </label>
+          ))}
         </div>
       </div>
 
@@ -414,6 +489,7 @@ export default function Form() {
         <select
           onChange={(e) => handleInputChange("paymentPreference", e.target.value)}
           value={formData.paymentPreference}
+          required
         >
           <option value="">Selecione</option>
           <option value="vista">À vista</option>
@@ -489,10 +565,16 @@ export default function Form() {
       <div className={styles.formGroup}>
         <label>De quanto você dispõe para aportes mensais (eventualidades e custos fixos)?</label>
         <input
-          type="number"
+          type="text"
           placeholder="Valor que dispõe"
           value={formData.monthlyContribution}
-          onChange={(e) => handleInputChange('monthlyContribution', e.target.value)}
+          onChange={(e) => {
+            // Remove qualquer coisa que não seja número ou ponto
+            const value = e.target.value.replace(/[^0-9,\.]/g, '');
+
+            // Substitui vírgula por ponto (caso a pessoa use vírgula como separador decimal)
+            handleInputChange('monthlyContribution', value.replace(',', '.'));
+          }}
         />
       </div>
 
@@ -520,16 +602,17 @@ export default function Form() {
         />
       </div>
 
-      {/* E-mail */}
-      <div className={styles.formGroup}>
-        <label>E-mail para confirmação:</label>
-        <input
-          type="email"
-          value={formData.email}
-          onChange={(e) => handleInputChange('email', e.target.value)}
-          required
-        />
-      </div>
+      {message && (
+        <div
+          className={`${styles.message} ${styles[message.type]}`}
+          ref={messageRef} // Attach the ref to the message container
+          tabIndex="-1" // Ensure the message can receive focus
+        >
+          {message.text}
+        </div>
+      )}
+
+      {loading && <div className={styles.loading}>Enviando...</div>}
 
       <button
         type="submit"
