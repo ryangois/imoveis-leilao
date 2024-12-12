954b708458fb2eded0e11da62e09f09c16ce9092
ryangois <ryangoisdev@gmail.com>
2024-12-12 14:29:19 -0300

    aqa

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -84,4 +84,7 @@ export default async function handler(req, res) {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Método ${req.method} não permitido.`);
     }
+    console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+
 }
